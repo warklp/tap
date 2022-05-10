045ca8b32f45cdd9607ce71bd4f22acfa5b1aead
warklp <49003984+warklp@users.noreply.github.com>
2022-05-10 18:21:37 -0300

    Update Telegram.php

--- includes/Telegram.php
+++ includes/Telegram.php
@@ -3067,7 +3067,8 @@ class Telegram
         $content = ['offset' => $offset, 'limit' => $limit, 'timeout' => $timeout];
         $this->updates = $this->endpoint('getUpdates', $content);
         if ($update) {
-            if (count($this->updates['result']) >= 1) { //for CLI working.
+   //         if (count($this->updates['result']) >= 1) { //for CLI working.
+            if ((\is_array($this->updates['result']) || \is_object($this->updates['result'])) && count($this->updates['result']) > 0) {
                 $last_element_id = $this->updates['result'][count($this->updates['result']) - 1]['update_id'] + 1;
                 $content = ['offset' => $last_element_id, 'limit' => '1', 'timeout' => $timeout];
                 $this->endpoint('getUpdates', $content);
